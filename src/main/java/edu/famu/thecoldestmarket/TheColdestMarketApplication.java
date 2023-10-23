8b2d9b334ee1b781c19c41ff6dce35109b4bbec2
Keisha Moreau <keisha1.moreau@famu.edu>
2023-10-23 10:06:13 -0400

    initial commit

---
+++ src/main/java/edu/famu/thecoldestmarket/TheColdestMarketApplication.java
@@ -0,0 +1,39 @@
+package edu.famu.thecoldestmarket;
+
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@SpringBootApplication
+public class TheColdestMarketApplication {
+
+    public static void main(String[] args) throws IOException {
+
+        //This line may be different based on what your project is named. Use the appropriate class name appears above
+        ClassLoader loader = TheColdestMarketApplication.class.getClassLoader();
+
+        //opens the file stored in resources
+        File file = new File(loader.getResource("ServiceAccountKey.json").getFile());
+
+        //reads the data from the file
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+        //connect to Firebase
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        if(FirebaseApp.getApps().isEmpty())
+            FirebaseApp.initializeApp(options);
+
+        SpringApplication.run(TheColdestMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/famu/thecoldestmarket/TheColdestMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.famu.thecoldestmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheColdestMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
