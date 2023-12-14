45b7b997bb344a76e35dfb8402d3fe6e7741b2d2
reynoldtiger796 <51296089+reynoldtiger796@users.noreply.github.com>
2023-12-14 13:08:56 -0500

    Commit my staged changes

--- frontend/temp/src/components/Menu.js
+++ frontend/temp/src/components/Menu.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-function Menu(props) {
+function Menu() {
     return (
         <nav className="navbar navbar-expand-md bg-body py-3">
             <div className="container">
-                <div id="navcol-2" className="collapse navbar-collapse"></div>
+                <div id="navcol-2" className="collapse navbar-collapse">
+                    <ul className="navbar-nav ms-auto" style={{ listStyleType: 'none', color: 'white' }}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/signup" className="nav-link">Sign Up</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/map" className="nav-link">Map</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/account" className="nav-link">Account</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/settings" className="nav-link">Settings</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/results" className="nav-link">Results</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
