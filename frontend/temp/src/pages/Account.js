9799bad5c358c0e26e92ee4b20ba9286f9291d5d
jalynmb <jalyn1.baker@famu.edu>
2023-12-14 21:06:43 -0500

    Account page frontend changes

--- frontend/temp/src/pages/Account.js
+++ frontend/temp/src/pages/Account.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../App.css';
+import profpic from '../profile.png';
 function Account() {
     return (
         <div className="container mt-5">
             <h1>Account</h1>
             <div className="card" style={{ width: '18rem' }}>
                 {/* User profile image */}
-                <img src="https://placekitten.com/200/200" className="card-img-top" alt="Profile" />
+                <img src={profpic} className="card-img-top align-items-center justify-content-center" alt="Profile" />
                 <div className="card-body">
                     {/* User details */}
-                    <h5 className="card-title">John Doe</h5>
+                    <h2 className="card-title">John Doe</h2>
                     <p className="card-text">Username: john doe123</p>
                     <p className="card-text">Email: john.doe@example.com</p>
                     <p className="card-text">Phone Number: (555) 123-4567</p>
@@ -20,15 +21,15 @@ function Account() {
                 {/* Change password form */}
                 <h3>Change Password</h3>
                 <form>
-                    <div className="mb-3">
+                    <div className="mb-3 custom-space">
                         <label htmlFor="currentPassword" className="form-label">Current Password</label>
                         <input type="password" className="form-control" id="currentPassword" />
                     </div>
-                    <div className="mb-3">
+                    <div className="mb-3 custom-space">
                         <label htmlFor="newPassword" className="form-label">New Password</label>
                         <input type="password" className="form-control" id="newPassword" />
                     </div>
-                    <div className="mb-3">
+                    <div className="mb-3 custom-space">
                         <label htmlFor="confirmPassword" className="form-label">Confirm New Password</label>
                         <input type="password" className="form-control" id="confirmPassword" />
                     </div>
